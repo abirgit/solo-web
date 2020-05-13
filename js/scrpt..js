e9cb123db1780ea0c22ae9d06b87c660c0c5099d
abir ahmed <abirahmed@gmail.com>
2020-05-14 01:24:00 +0600

    13 may

--- js/scrpt..js
+++ js/scrpt..js
@@ -3,3 +3,21 @@ $(window).on('load', function () { // make sURE WHOLe website is loaded//
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+/* Team */
+
+$(function() {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        
+        
+        
+    });
+});
+    
