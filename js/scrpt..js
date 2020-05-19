0c8b21d7c80e4f0f9225559b701023a8ff2b48ba
abir ahmed <abirahmed@gmail.com>
2020-05-19 20:40:49 +0600

    19

--- js/scrpt..js
+++ js/scrpt..js
@@ -23,46 +23,29 @@ $(function () {
 /* Progress Bars */
 
 $(function () {
-
-
     $("#progress-elements").waypoint(function () {
-
-
         $(".progress-bar").each(function () {
-
             $(this).animate({
                 width: $(this).attr("aria-valuenow") + "%"
             }, 800);
-
-
         })
         this.destroy();
     }, {
         offset: 'bottom-in-view'
-
     });
-
-
-
-
-
-
-
-
-
-    /*$(function() {
-        $(".progress-bar").each(function() {
-            
-            $(this).animate({width: $(this).attr("aria-valuenow") + "%"
-                            },1000);
-            
-            
-        }); */
-
-
-
-
-
-
-
 });
+
+
+
+
+
+/* Responsive Tabs */
+    
+$(function () {
+    $("#services-tabs").responsiveTabs({
+       animation:'slide'
+    });
+});
+
+
+
